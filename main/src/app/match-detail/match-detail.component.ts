f93c5acbb799e413ec3bd6304e8a897e14decb1e
HouPoc <hou199488@gmail.com>
2019-02-27 23:54:42 -0800

    set up layout for picked heroes

--- main/src/app/match-detail/match-detail.component.ts
+++ main/src/app/match-detail/match-detail.component.ts
@@ -10,13 +10,19 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class MatchDetailComponent implements OnInit {
   request: any;
   server_steam_id: any;
+  heroes: any;
   constructor(private matchDetail: ActivatedRoute, private matchList: Router) { }
 
   ngOnInit() {
     this.request = this.matchDetail.params.subscribe(params => {
       this.server_steam_id = params;
+      this.heroes = ['../../assets/heroes/onPage/abaddon.png', 
+                     '../../assets/heroes/onPage/alchemist.png',
+                     '../../assets/heroes/onPage/axe.png', 
+                     '../../assets/heroes/onPage/chen.png', 
+                     '../../assets/heroes/onPage/clinkz.png',  
+                    ];
       console.log(this.server_steam_id);
     });
   }
-
 }
