eddd9ee960e5460cbdb5b14448f9a25249ccaef1
HouPoc <hou199488@gmail.com>
2019-03-14 19:06:52 -0700

    loading data to front end

--- main/src/app/match-detail/match-detail.component.ts
+++ main/src/app/match-detail/match-detail.component.ts
@@ -3,25 +3,20 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { MatSort, MatTableDataSource} from '@angular/material';
 import { GetMatchDetailService } from '../get-match-detail.service';
 
-export interface PeriodicElement {
+export interface Player {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  icon: string;
+  level: number;
+  kill: number;
+  death: number;
+  assist: number;
+  deny: number;
+  last_hit: number;
+  gold: number;
+  net_worth: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+
 
 @Component({
   selector: 'app-match-detail',
@@ -31,18 +26,37 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 
 export class MatchDetailComponent implements OnInit {
-  matchDetail: any;
 
+  matchDetail: any;
   interval: any;
   request: any;
   server_steam_id: any;
-  heroes: any;
-  bans: any;
-  public lineChartData:Array<any> = [
+  // The following is about radiant
+  radiant_score: number;
+  radiant_heroes: any;
+  radiant_bans: any;
+  radiant_towers: any;
+  radiant_barracks: any;
+  radiant_base: any;
+  // The following is about dire
+  dire_score: number;
+  dire_heroes: any;
+  dire_bans: any;
+  dire_towers: any;
+  dire_barracks: any;
+  dire_base: any;
+
+  // The following is about match MATA DATA
+  time: number;
+  graph_data: any[];
+  Players: Player[] = [];
+
+
+  public lineChartData: Array<any> = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Team Worth Difference'},
   ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartColors:Array<any> = [
+  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartColors: Array<any> = [
     { // grey
       backgroundColor: 'rgba(148,159,177,0.2)',
       borderColor: 'rgba(148,159,177,1)',
@@ -52,8 +66,8 @@ export class MatchDetailComponent implements OnInit {
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     },
   ];
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
+  public lineChartLegend = true;
+  public lineChartType = 'line';
 
 
 
@@ -63,29 +77,104 @@ export class MatchDetailComponent implements OnInit {
               ) { }
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource = new MatTableDataSource(this.Players);
   @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit() {
-    this.heroes = ['../../assets/heroes/onPage/abaddon.png',
-                   '../../assets/heroes/onPage/alchemist.png',
-                   '../../assets/heroes/onPage/axe.png',
-                   '../../assets/heroes/onPage/chen.png',
-                   '../../assets/heroes/onPage/clinkz.png',
-                  ];
-    this.bans = '../../assets/heroes/onPage/default.png';
-
+    // Get Server Steam ID from previous page.
     this.request = this.match.params.subscribe(params => {
       this.server_steam_id = params.server_steam_id;
       console.log(this.server_steam_id);
       this.dataSource.sort = this.sort;
     });
+
+    // Call service to get match data with server steam id as query key
     this.interval = setInterval(() => {
       this.getMatchDetail.getMatchDetail(this.server_steam_id).subscribe(match_detail => {
         this.matchDetail = match_detail;
+
       });
-      console.log(this.matchDetail);
     }, 5000);
 
   }
+
+  extractData() {
+    // collect match mata data
+    this.time = this.matchDetail.time;
+    this.graph_data = this.matchDetail.worth_graph;
+    let i = 0;
+    for (const p of this.matchDetail.radiant.players) {
+      const tmp = {
+        'name': p.name,
+        'level': p.level,
+        'kill': p.kill_count,
+        'death': p.death_count,
+        'assist': p.assists_count,
+        'deny': p.denies_count,
+        'gold': p.gold,
+        'net_worth': p.net_worth,
+        'last_hit': p.lh_count,
+        'icon': this.matchDetail.picks.radiant[i].icon
+      };
+      this.Players.push(tmp);
+      i = i + 1;
+    }
+
+    for (const p of this.matchDetail.dire.players) {
+      const tmp = {
+        'name': p.name,
+        'level': p.level,
+        'kill': p.kill_count,
+        'death': p.death_count,
+        'assist': p.assists_count,
+        'deny': p.denies_count,
+        'gold': p.gold,
+        'net_worth': p.net_worth,
+        'last_hit': p.lh_count,
+        'icon': this.matchDetail.picks.dire[i].icon
+      };
+      this.Players.push(tmp);
+      i = i + 1;
+    }
+    // Net worth line
+    this.lineChartData = [{data: this.graph_data, label: 'Team Worth Difference'}];
+    this.lineChartLabels = this.range(0, this.time, this.graph_data.length);
+
+    // collect radiant_data
+    this.radiant_score = this.matchDetail.radiant.score;
+    this.radiant_heroes = this.matchDetail.picks.radiant;
+    this.radiant_bans = this.matchDetail.bans.radiant;
+    this.radiant_towers = this.matchDetail.radiant.buildings[0];
+    this.radiant_barracks = this.matchDetail.radiant.buildings[1];
+    this.radiant_base = this.matchDetail.radiant.buildings[2][0];
+
+    // collect dire_data
+    this.dire_score = this.matchDetail.radiant.score;
+    this.dire_heroes = this.matchDetail.picks.dire;
+    this.dire_bans = this.matchDetail.bans.dire;
+    this.dire_towers = this.matchDetail.radiant.buildings[0];
+    this.dire_barracks = this.matchDetail.radiant.buildings[1];
+    this.dire_base = this.matchDetail.radiant.buildings[2][0];
+  }
+
+  range(_p: number, _t?: number, _s?: number): Array<number> {
+    /**
+     * @param <_p> Return a list integers of zero until <_p> value.
+     * @param <_t> Return a list integers of <_t> until <_p> value.
+     * @param <_s> Return a list integers of <_t> until <_p> with steps <_s> value.
+     * @return Return a array list
+     */
+
+    let start: number = (_t) ? _p : 0;
+    let stop: number = (_t) ? _t : _p;
+    let step: number = (_s) ? _s : 1;
+
+    let t: Array<number> = [];
+    for (let i = start; i < stop; i = i + step) {
+      t.push(i);
+    }
+
+    return t;
+  }
 }
+
